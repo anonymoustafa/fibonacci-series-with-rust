7e2e73e8cfc478ebe5003fc0ff5056fb0b1a9b5f
Mostafa <darthmostafa@gmail.com>
2020-06-12 03:56:47 +0430

    initialized

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+
+fn main() {
+    println!("Input the fibonacci index you want me to ask for calculation ");
+    let mut fibonacci_index = String::new();
+    io::stdin()
+        .read_line(&mut fibonacci_index)
+        .expect("Failed to read line");
+    let fibonacci_index: u32 = match fibonacci_index.trim().parse() {};
+    for i in 1..fibonacci_index {
+        if fibonacci_index <= 0 {println!("error, index must be zero or bigger.");}
+        else if fibonacci_index == 0 || fibonacci_index == 1 {println!("1");}
+        else { println!("{}", fibonacci_series);}
+    }
+}
+
+fn fibonacci_series(fibonacci_index: us32) -> usize {
+    let mut index = 0;
+
+}
