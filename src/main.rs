87535f11db19a0b890577f88fdbc8a7be988f3a6
Mostafa <darthmostafa@gmail.com>
2020-06-12 17:31:42 +0430

    nothing

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ fn main() {
     io::stdin()
         .read_line(&mut fibonacci_index)
         .expect("Failed to read line");
-    let fibonacci_index: u32 = match fibonacci_index.trim().parse() {};
+    let fibonacci_index: u32 = return match fibonacci_index.trim().parse() {
+        Ok(_) => {},
+        Err(_) => {},
+    };
     for i in 1..fibonacci_index {
         if fibonacci_index <= 0 {println!("error, index must be zero or bigger.");}
         else if fibonacci_index == 0 || fibonacci_index == 1 {println!("1");}
